78c44dcb9d0d9c302d51693d78d4c11267b2947e
Ｅd <Ed@ddeMacBook-Air.local>
2019-12-07 21:35:14 +0800

    Add rate limiting

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const limiters = require('./limiters');
 const routes = require('./routes');
 const app = express();
 
+limiters.apply(app);
 app.use('/', routes);
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
---
+++ limiters.js
@@ -0,0 +1,59 @@
+const rateLimit = require('express-rate-limit');
+const _ = require('lodash');
+
+const LIMITERS = [
+    {
+        name: 'ip',
+        windowMs: 1 * 60 * 1000,
+        limit: 10,
+        keyGenerator: req => req.ip
+    },
+    {
+        name: 'id',
+        windowMs: 1 * 60 * 1000,
+        limit: 5,
+        keyGenerator: req => req.query.user
+    }
+];
+
+module.exports = {
+    apply: app => {
+        _.forEach(LIMITERS, (item, i) => {
+            let limiter = rateLimit({
+                windowMs: item.windowMs,
+                max: item.limit,
+                keyGenerator: req => {
+                    if(i > 0) {
+                        req[`limit_${LIMITERS[i - 1].name}`] = req.rateLimit.current;
+                    }
+                    return item.keyGenerator(req);
+                },
+                handler: (req, res, next) => next() //do nothing, because we want to get current value for each limiter
+            });
+            app.use(limiter);
+        });
+        app.use((req, res, next) => {
+            let LimitReached = false;
+            let result = {};
+            _.forEach(LIMITERS, (item, i) => {
+                if(i === LIMITERS.length - 1) {
+                    result[item.name] = req.rateLimit.current;
+                    if(req.rateLimit.current > item.limit) {
+                        LimitReached = true;
+                    }
+                }else {
+                    let current = req[`limit_${item.name}`];
+                    result[item.name] = current;
+                    if(current > item.limit) {
+                        LimitReached = true;
+                    }
+                }       
+            });
+            if(LimitReached) {
+                res.status(403).send(result);
+            }else {
+                next();
+            }
+        });
+    }
+};
